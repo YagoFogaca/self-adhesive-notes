5020e9ece48494b65ec6ba2cf997f3a0d631af65
YagoFogaca <yagofogaca20@gmai.com>
2023-07-10 18:53:21 -0300

    feat: implementação da página de login

---
+++ resources/views/pages/login/index.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.index')
+
+@section('content')
+    @extends('components.card-form.index')
+@section('form')
+    <section class="section-form">
+        <form action="" method="POST">
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Email</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Seu Email">
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlTextarea1" class="form-label">Senha</label>
+                <input type="password" class="form-control" id="exampleFormControlInput1" placeholder="Sua senha">
+            </div>
+            <div class="d-grid gap-2">
+                <button class="btn btn-primary" type="button">Login</button>
+            </div>
+        </form>
+    </section>
+@endsection
+@endsection
