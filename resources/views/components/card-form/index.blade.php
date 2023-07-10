edf31a700eb10ae01fe49138de8f22ad19431134
YagoFogaca <yagofogaca20@gmai.com>
2023-07-10 18:44:16 -0300

    feat: componente card para formularios

---
+++ resources/views/components/card-form/index.blade.php
@@ -0,0 +1,7 @@
+<section class="card-form">
+    <article class="card-article">
+        <h2>Esqueça papeis!!</h2>
+        <p> Use as notas autoadesivas online e gratuitas</p>
+    </article>
+    @yield('form')
+</section>
