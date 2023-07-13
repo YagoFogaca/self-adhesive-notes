b437f8a27d0b96d52b4b1161323edc5374d7dfc9
YagoFogaca <yagofogaca20@gmai.com>
2023-07-13 11:28:52 -0300

    style: implementação da logo

--- resources/views/components/nav/index.blade.php
+++ resources/views/components/nav/index.blade.php
@@ -1,6 +1,8 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">Logo</a>
+        <a class="navbar-brand" href="#">
+            <img src={{ asset('./assets/imgs/logo.png') }} style="width: 40px;" alt="Logo">
+        </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -8,10 +10,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                    <button class="nav-link btn-open" type="button">Criar</button>
+                    <button class="nav-link active btn-open" type="button">Criar</button>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href={{ route('logout') }}>Sair</a>
